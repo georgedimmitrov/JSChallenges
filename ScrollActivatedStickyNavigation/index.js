819450e96f6211e531ef4641efef1fb521e6aea7
Georgi Dimitrov <georgedimmitrov@gmail.com>
2021-08-04 22:18:26 +0300

    Sticky navigation shown on scroll up

---
+++ ScrollActivatedStickyNavigation/index.js
@@ -0,0 +1,23 @@
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', (event) => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    body.classList.remove('scroll-up');
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-up');
+    body.classList.add('scroll-down');
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains('scroll-down')
+  ) {
+    body.classList.add('scroll-up');
+    body.classList.remove('scroll-down');
+  }
+
+  lastScroll = currentScroll;
+});
